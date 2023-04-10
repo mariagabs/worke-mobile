02d458cf63f281a7d864323e6da5618e67bd933b
Raphael Konichi <raphael.konichi@gmail.com>
2023-04-09 21:54:46 -0300

    Changes IA camera
    Erros na hora de converter o tensor para usar o modelo de predição
    
    Sem a conversão:
    [Unhandled promise rejection: Error: Error when checking : expected rescaling_1_input to have 4 dimension(s), but got array with shape [180,180,3]]
    
    Com a conversão:
    [Unhandled promise rejection: Error: setValueAtIndex: index 1 is out of bounds [0, 1)]

--- worke-mobile/src/components/pages/Exercise.tsx
+++ worke-mobile/src/components/pages/Exercise.tsx
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { Camera, CameraType, FlashMode } from "expo-camera";
-import { cameraWithTensors, bundleResourceIO } from "@tensorflow/tfjs-react-native";
-import { getModel } from '../../model/utils.js'
+import * as tf from "@tensorflow/tfjs"
+import '@tensorflow/tfjs-react-native';
+import { cameraWithTensors, bundleResourceIO, fetch } from "@tensorflow/tfjs-react-native";
+// import { getModel } from '../../../assets/model/model.json'
 
 const TensorCamera = cameraWithTensors(Camera);
 
 const Exercise: React.FC = () => {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [imageDetector, setImageDetector] = useState(null);
+  const [showCamera, setShowCamera] = useState(false);
+
+  useEffect(() => {
+    setShowCamera(true);
+
+    return () => {
+      setShowCamera(false);
+    }
+  }, []);
 
   useEffect(() => {
     const getPermission = async () => {
@@ -16,6 +28,22 @@ const Exercise: React.FC = () => {
     };
 
     getPermission();
+
+    async function loadModel(){
+      console.log('iniciou loadModel');
+      const tfReady = await tf.ready();
+      const modelJson = require('../../../assets/model/model.json');
+      const modelWeights = require('../../../assets/model/group1-shard.bin');
+
+      const thisModel = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeights));
+      console.log('thisModel');
+      // console.log('tfjs');
+      // console.log(tf.version);
+
+      setImageDetector(thisModel);
+      console.log('finalizou loadModel');
+    }
+    loadModel();
 
     console.log(permission);
   }, []);
@@ -26,17 +54,32 @@ const Exercise: React.FC = () => {
     );
   }
 
-  function handleCameraStream(images, updatePreview, gl) {
-    const loop = async () => {
-      const nextImageTensor = images.next().value;
-
-
-      const myModel = await getModel();
+  function handleCameraStream(tensors) {
     
-      const poses = await myModel.predict(images);
-      //
+    const loop = async () => {
+      const nextImageTensor = tensors.next().value;
+
+      console.log(JSON.stringify(nextImageTensor));
+      // const axs = 0;
+      // const imageTensorReshaped = nextImageTensor.expandDims(axs);
+
+      const expandedImageTensor = tf.reshape(nextImageTensor, [1,180,180,3]);
+      console.log(JSON.stringify(expandedImageTensor));
+      console.log('expandedImageTensor');
+      console.log('starting prediction');
+
+      const predictions = await imageDetector.predict(nextImageTensor);
+      
+      console.log(predictions.dataSync());
+      // const newImage = nextImageTensor.reshape([1,180,180,3]);
+      // let result = await imageDetector.predict(newImage);
+
+      // console.log(JSON.stringify(result));
+    
+      // tf.dispose(tensors);
+      
       // do something with tensor here
-      //
+      
 
       // if autorender is false you need the following two lines.
       // updatePreview();
@@ -50,19 +93,21 @@ const Exercise: React.FC = () => {
   let camera: Camera;
   return (
     <View style={styles.container}>
-      <TensorCamera
-        // Standard Camera props
-        style={{ flex: 1, width: "100%" }}
-        type={CameraType.front}
-        // Tensor related props
-        resizeHeight={200}
-        resizeWidth={152}
-        resizeDepth={3}
-        onReady={handleCameraStream}
-        autorender={true}
-        autoFocus={true}
-        flashMode={FlashMode.off}
-      />
+      {showCamera && (
+        <TensorCamera
+          // Standard Camera props
+          style={{ flex: 1, width: "100%" }}
+          type={CameraType.front}
+          // Tensor related props
+          resizeHeight={180}
+          resizeWidth={180}
+          resizeDepth={3}
+          onReady={handleCameraStream}
+          autorender={true}
+          autoFocus={true}
+          flashMode={FlashMode.off}
+        />
+      )}
       {/* <Camera
         ref={(r) => {
           camera = r;
