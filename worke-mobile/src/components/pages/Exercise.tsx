e500b584c74889b954ffc53a0d5248992d7ce261
mariagabs <71193719+mariagabs@users.noreply.github.com>
2023-04-15 12:26:13 -0300

    Atualizações pro pc

--- worke-mobile/src/components/pages/Exercise.tsx
+++ worke-mobile/src/components/pages/Exercise.tsx
@@ -1,10 +1,25 @@
-import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
+import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
 import React, { useState, useEffect, useRef } from "react";
-import { View, TouchableOpacity, Text, StyleSheet, SafeAreaView, Button, Image, Platform } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Image,
+  Platform,
+} from "react-native";
 import { Camera, CameraType, FlashMode } from "expo-camera";
-import * as tf from '@tensorflow/tfjs'
-import '@tensorflow/tfjs-react-native';
-import { cameraWithTensors, bundleResourceIO, fetch, decodeJpeg } from "@tensorflow/tfjs-react-native";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-react-native";
+import {
+  cameraWithTensors,
+  bundleResourceIO,
+  fetch,
+  decodeJpeg,
+} from "@tensorflow/tfjs-react-native";
+import { createNativeWrapper } from "react-native-gesture-handler";
 // import * as MediaLibrary from 'expo-media-library';
 
 const TensorCamera = cameraWithTensors(Camera);
@@ -21,89 +36,100 @@ const Exercise: React.FC = () => {
 
     return () => {
       setShowCamera(false);
-    }
+    };
   }, []);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
+      setHasPermission(status === "granted");
 
-    async function loadModel(){
+      // async function loadModel() {
       const tfReady = await tf.ready();
-      const modelJson = require('../../../assets/model/model.json');
-      const modelWeight = require('../../../assets/model/group1-shard.bin');
+      const modelJson = require("../../../assets/model/model.json");
+      const modelWeight = require("../../../assets/model/group1-shard.bin");
       // const modelJson = require('../../../assets/model/modellib.json');
       // const modelWeight = require('../../../assets/model/group-shardlib.bin');
 
-      console.log('iniciou loadModel');
+      console.log("iniciou loadModel");
       // const thisModel = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeight));
 
       const thisModel = await tf.loadGraphModel(
-        bundleResourceIO(modelJson, modelWeight));
-      console.log('modelo is on');
+        bundleResourceIO(modelJson, modelWeight),
+      );
 
-      console.log('thisModel');
-      console.log(tf.version);
+      await sleep(5000);
 
-      setImageDetector(thisModel);
-      console.log('finalizou loadModel');
-    }
-    loadModel();
-  }, []);
+      console.log("modelo is on");
+
+      await setImageDetector(thisModel);
+
+      console.log("finalizou loadModel: ", imageDetector);
+      // }
+      // loadModel();
+    })();
+  }, [imageDetector]);
+
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
 
   async function handleCameraStream(images) {
-    
     const loop = async () => {
-
       const nextImageTensor = images.next().value;
-      console.log(JSON.stringify(nextImageTensor));
+      // console.log(JSON.stringify(nextImageTensor));
       nextImageTensor.print();
 
       // const resized = tf.image.resizeBilinear(nextImageTensor, [640, 480]);
       // // resized.isDisposedInternal = true;
-  
+
       // const casted = resized.cast('int32');
-  
+
       // const expanded = casted.expandDims(0);
-  
-  
+
       // // faz a previsão.
       // const obj = await imageDetector.executeAsync(expanded);
 
-      const resized = tf.image.resizeBilinear(nextImageTensor, [224,224]);
+      const resized = tf.image.resizeBilinear(nextImageTensor, [224, 224]);
       console.log(JSON.stringify(resized));
-      
-      const casted = tf.cast(resized, 'float32');
-      console.log(JSON.stringify(casted));
 
-      const expanded = tf.expandDims(casted, 0)
-      console.log(JSON.stringify(expanded));
+      const casted = tf.cast(resized, "float32");
+      // console.log(JSON.stringify(casted));
+
+      const expanded = tf.expandDims(casted, 0);
+      // console.log(JSON.stringify(expanded));
       expanded.print();
 
       // const expandedImageTensor = tf.reshape(nextImageTensor, [1,224,224,3]);
       // console.log(JSON.stringify(expandedImageTensor));
       // expandedImageTensor.print();
-  
+
       // const inputs = tf.ones([1, 224, 224, 3]);
-      
+
       // inputs.print();
-      const obj = await imageDetector.executeAsync(expanded);
-      console.log(obj.dataSync());
+      await sleep(5000);
+      console.log("imageDetector", imageDetector);
+      const obj = imageDetector.execute(expanded);
+      console.log(obj.data());
 
       // console.log(predictions.dataSync());
-
+      tf.dispose(images);
       requestAnimationFrame(loop);
+      await sleep(5000);
     };
+
     loop();
-  };
+  }
 
   const textureDims =
-    Platform.OS === 'ios'
+    Platform.OS === "ios"
       ? { width: 1080, height: 1920 }
       : { width: 1600, height: 1200 };
-      
+
   let camera: Camera;
   return (
     <View style={styles.container}>
@@ -121,7 +147,7 @@ const Exercise: React.FC = () => {
           zoom={0}
           resizeDepth={3}
           onReady={handleCameraStream}
-          autorender={true}
+          autorender={false}
           autoFocus={true}
           flashMode={FlashMode.off}
         />
@@ -135,13 +161,13 @@ const Exercise: React.FC = () => {
       ></Camera> */}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "#fff",
   },
 });
